aaf55546a45a115451a2339b05a0c1c5d55e5a68
tokujiTO <tiagomassuda123@gmail.com>
2025-02-25 08:57:03 -0300

    aula1TEO

--- TEORIA/01/EstudoDesempenho.java
+++ TEORIA/01/EstudoDesempenho.java
@@ -1,15 +1,34 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class EstudoDesempenho {
+  static void preencheVetor(int[] v) {
+    Random random = new Random();
+    for (int i = 0; i < v.length; i++) {
+      v[i] = random.nextInt(v.length * 10);
+    }
+  }
+
+  static void exibeVetor(int[] v) {
+    System.out.println();
+    for (int i = 0; i < v.length; i++) {
+      System.out.print(v[i] + " ");
+    }
+  }
   public static void main(String[] args) {
+
     int[] v;
     Scanner scanner = new Scanner(System.in);
     int tamanho = 0;
-    do {
-      System.out.println("Digite o tamanho do vetor(0 encerra): \n");
-      tamanho = scanner.nextInt();
+    System.out.println("Digite o tamanho do vetor(0 encerra): \n");
+    tamanho = scanner.nextInt();
+    while (tamanho > 0) {
       v = new int[tamanho];
-    } while (tamanho > 0);
+      preencheVetor(v);
+      exibeVetor(v);
+      System.out.println("\nDigite o tamanho do vetor(0 encerra): \n");
+      tamanho = scanner.nextInt();
+    };
     scanner.close();
   }
 }
