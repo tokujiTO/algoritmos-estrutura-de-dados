40ddeb16c8fad306b54fa620b2fa4862c9962f2a
tokujiTO <tiagomassuda123@gmail.com>
2025-03-18 09:18:03 -0300

    aula 03

--- TEORIA/03/TesteNo.java
+++ TEORIA/03/TesteNo.java
@@ -1,6 +1,9 @@
 public class TesteNo {
   public static void main(String[] args) {
     No no1, no2;
-    
+    no1 = new No(5);
+    no2 = new No(8);
+    System.out.println("No1: " + no1);
+    System.out.println("No2: " + no2);
   }
 }
