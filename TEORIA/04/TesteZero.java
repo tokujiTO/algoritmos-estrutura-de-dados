8f1faff898a4ccb735713bc4cefc2bb063d8aef5
tokujiTO <tiagomassuda123@gmail.com>
2025-03-25 08:23:43 -0300

    feat: aula 04

---
+++ TEORIA/04/TesteZero.java
@@ -0,0 +1,7 @@
+public class TesteZero {
+  public static void main(String[] args) {
+    // System.out.println("Divisão inteira: " + 1/0);
+    double x = 1, y = 0;
+    System.out.println("Divisão Double: " + x/y);
+  }
+}
