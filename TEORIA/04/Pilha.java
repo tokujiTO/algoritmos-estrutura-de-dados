733e5904457c8d830d5b7a916ee015ce65cc9543
tokujiTO <tiagomassuda123@gmail.com>
2025-03-25 09:18:14 -0300

    feat: aula 04

--- TEORIA/04/Pilha.java
+++ TEORIA/04/Pilha.java
@@ -1,6 +1,43 @@
 
 public class Pilha {
-  public static void main(String[] args) {
-    
+  private int topo;
+  private int[] dados;
+
+  public Pilha() {
+    this(10);
+  }
+
+  public Pilha(int capacidade) {
+    topo = 0;
+    dados = new int[capacidade];
+  }
+
+  public boolean pilhaVazia() {
+    return topo == 0;
+  }
+
+  public boolean pilhaCheia() {
+    return topo == dados.length;
+  }
+
+  public boolean push(int elemento) {
+    if (pilhaCheia()) {
+      return false;
+    }
+
+    dados[topo++] = elemento;
+    return true;
+  }
+
+  public int pop() {
+    if (pilhaCheia())
+      return -1;
+    return dados[--topo];
+  }
+
+  public int peek() {
+    if (pilhaVazia())
+      return -1;
+    return dados[topo - 1];
   }
 }
