9b9615da052d41589c89fc805572fe76aaba4211
tokujiTO <tiagomassuda123@gmail.com>
2025-03-26 19:46:16 -0300

    atividade 1

---
+++ ATIVIDADES/atividade1/NossoVetor.java
@@ -0,0 +1,112 @@
+import java.util.Random;
+
+public class NossoVetor {
+    private int ocupacao;
+    private int[] vetor;
+
+    public NossoVetor(int tamanho) {
+        vetor = new int[tamanho];
+        ocupacao = 0; // por clareza
+    }
+
+    public NossoVetor() {
+      this(10);
+    }
+    
+    public void insere(int i) {
+        if (estaCheio())
+            redimensionaVetor(vetor.length * 2);
+        vetor[ocupacao++] = i;
+    }
+
+    boolean estaCheio() {
+        return ocupacao == vetor.length;
+    }
+
+    public boolean estaVazio() {
+        return ocupacao == 0;
+    }
+
+    public int remove() {
+        if (estaVazio())
+            throw new VetorVazioException("vetor vazio, nao ha o que remover");
+        int i = vetor[--ocupacao];
+        if (vetor.length >= 4 && ocupacao <= vetor.length / 4)
+            redimensionaVetor(vetor.length / 2);
+        return i;
+    }
+
+    public String toString() {
+        String s = "ocupacao = " + ocupacao + "\n";
+        for (int i = 0; i < ocupacao; i++) {
+            s += vetor[i] + " ";
+        }
+        return s + "\n";
+    }
+
+    private void redimensionaVetor(int novoTamanho) {
+        int[] temp = new int[novoTamanho];
+        for (int i = 0; i < ocupacao; i++)
+            temp[i] = vetor[i];
+        vetor = temp;
+    }
+
+    public int getTamanho() {
+        return vetor.length;
+    }
+
+    public void preencheVetor() {
+        Random random = new Random();
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = random.nextInt(vetor.length * 10);
+        }
+        ocupacao = vetor.length;
+    }
+
+    public long bubbleSort() {
+    long counter = 0;
+    boolean trocou;
+    for (int i = 0; i < ocupacao - 1; i++) {  // Usar ocupacao, não vetor.length
+        trocou = false;
+        for (int j = 0; j < ocupacao - i - 1; j++) {  // Limite corrigido
+            if (vetor[j] > vetor[j + 1]) {
+                int aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+                counter++;
+                trocou = true;
+            }
+        }
+        if (!trocou) break;  // Otimização: se não houve trocas, o vetor já está ordenado
+    }
+    return counter;
+}
+
+    public void selectionsort() {
+      for (int i = 0; i < vetor.length - 1; ++i) {
+        int min = i;
+        for (int j = i + 1; j < vetor.length; ++j)
+          if (vetor[j] < vetor[min])
+            min = j;
+        int x = vetor[i];
+        vetor[i] = vetor[min];
+        vetor[min] = x;
+      }
+    }
+    
+    public void insertionSort() {
+        for (int j = 1; j < vetor.length; ++j) {
+            int x = vetor[j];
+            int i;
+            for (i = j - 1; i >= 0 && vetor[i] > x; --i)
+                vetor[i + 1] = vetor[i];
+            vetor[i + 1] = x;
+        }
+    }
+}
+
+class VetorVazioException extends RuntimeException {
+    public VetorVazioException(String msg) {
+        super(msg);
+    }
+}
---
+++ ATIVIDADES/atividade1/Teste.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Teste {
+  public static void main(String[] args) {
+    // vamos testar a eficácia dos métodos de ordenação
+    Scanner scanner = new Scanner(System.in);
+    int tamanho = 0;
+    do {
+      System.out.println("Selecione um tamanho: \n");
+      System.out.println("[1] - 100m\n");
+      System.out.println("[2] - 200m\n");
+      System.out.println("[3] - 400m\n");
+      System.out.println("[4] - 800m\n");
+      System.out.println("[5] - 1,6M\n");
+      System.out.println("cancelar - 0\n");
+      
+      tamanho = scanner.nextInt();
+
+      switch (tamanho) {
+        case 1:
+          tamanho = 100000;
+          break;
+        case 2:
+          tamanho = 200000;
+          break;
+        case 3:
+          tamanho = 400000;
+          break;
+        case 4:
+          tamanho = 800000;
+          break;
+        case 5:
+          tamanho = 1600000;
+          break;
+        default:
+          System.out.println("Opção inválida");
+          break;
+      };
+      
+      if (tamanho == 0) {
+        break;
+      }
+
+      NossoVetor vetor = new NossoVetor(tamanho);
+
+      long soma = 0;
+      long tempoInicial = System.currentTimeMillis();
+      for (int i = 0; i < 30; i++) {
+        vetor.preencheVetor();
+        System.err.println("Iteração " + (i + 1) + ")");
+        long tempoInicialOrdenacao = System.currentTimeMillis();
+        long iteracoes = vetor.bubbleSort();
+        System.out.println("\nQuantidade de trocas: " + iteracoes);
+        long tempoFinalOrdenacao = System.currentTimeMillis();
+        System.out.println("Tempo de ordenação: " + (tempoFinalOrdenacao - tempoInicialOrdenacao) + "ms");
+        soma += iteracoes;
+      }
+      long tempoFinal = System.currentTimeMillis();
+      System.out.println("\nMédia de trocas: " + (soma / 30));
+      System.out.println("Tempo total: " + (tempoFinal - tempoInicial) + "ms");
+
+    } while (tamanho != 0);
+  }
+}
