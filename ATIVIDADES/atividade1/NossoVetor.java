23a4380481a47fabd91f576e326bd94fd912f435
tokujiTO <tiagomassuda123@gmail.com>
2025-04-15 09:06:56 -0300

    adding quick sort to nossoVetor

--- ATIVIDADES/atividade1/NossoVetor.java
+++ ATIVIDADES/atividade1/NossoVetor.java
@@ -1,171 +1,193 @@
 import java.util.Random;
 
 public class NossoVetor {
-    private int ocupacao;
-    private int[] vetor;
+  private int ocupacao;
+  private int[] vetor;
 
-    public NossoVetor(int tamanho) {
-        vetor = new int[tamanho];
-        ocupacao = 0; // por clareza
-    }
+  public NossoVetor(int tamanho) {
+    vetor = new int[tamanho];
+    ocupacao = 0; // por clareza
+  }
 
-    public NossoVetor() {
-      this(10);
-    }
-    
-    public void insere(int i) {
-        if (estaCheio())
-            redimensionaVetor(vetor.length * 2);
-        vetor[ocupacao++] = i;
-    }
+  public NossoVetor() {
+    this(10);
+  }
 
-    boolean estaCheio() {
-        return ocupacao == vetor.length;
-    }
+  public void insere(int i) {
+    if (estaCheio())
+      redimensionaVetor(vetor.length * 2);
+    vetor[ocupacao++] = i;
+  }
 
-    public boolean estaVazio() {
-        return ocupacao == 0;
-    }
+  boolean estaCheio() {
+    return ocupacao == vetor.length;
+  }
 
-    public int remove() {
-        if (estaVazio())
-            throw new VetorVazioException("vetor vazio, nao ha o que remover");
-        int i = vetor[--ocupacao];
-        if (vetor.length >= 4 && ocupacao <= vetor.length / 4)
-            redimensionaVetor(vetor.length / 2);
-        return i;
-    }
+  public boolean estaVazio() {
+    return ocupacao == 0;
+  }
 
-    public String toString() {
-        String s = "ocupacao = " + ocupacao + "\n";
-        for (int i = 0; i < ocupacao; i++) {
-            s += vetor[i] + " ";
+  public int remove() {
+    if (estaVazio())
+      throw new VetorVazioException("vetor vazio, nao ha o que remover");
+    int i = vetor[--ocupacao];
+    if (vetor.length >= 4 && ocupacao <= vetor.length / 4)
+      redimensionaVetor(vetor.length / 2);
+    return i;
+  }
+
+  public String toString() {
+    String s = "ocupacao = " + ocupacao + "\n";
+    for (int i = 0; i < ocupacao; i++) {
+      s += vetor[i] + " ";
+    }
+    return s + "\n";
+  }
+
+  private void redimensionaVetor(int novoTamanho) {
+    int[] temp = new int[novoTamanho];
+    for (int i = 0; i < ocupacao; i++)
+      temp[i] = vetor[i];
+    vetor = temp;
+  }
+
+  public int getTamanho() {
+    return vetor.length;
+  }
+
+  public void esvaziaVetor() {
+    for (int i = 0; i < vetor.length; i++) {
+      vetor[i] = 0;
+    }
+    ocupacao = 0;
+  }
+
+  public void preencheVetor() {
+    Random random = new Random();
+    for (int i = 0; i < vetor.length; i++) {
+      vetor[i] = random.nextInt(vetor.length * 10);
+    }
+    ocupacao = vetor.length;
+  }
+
+  public long bubbleSort() {
+    long counter = 0;
+    boolean trocou = true;
+    for (int i = 0; i < ocupacao - 1 && trocou; i++) {
+      trocou = false;
+      for (int j = 0; j < ocupacao - i - 1; j++) {
+        counter++;
+        if (vetor[j] > vetor[j + 1]) {
+          int aux = vetor[j];
+          vetor[j] = vetor[j + 1];
+          vetor[j + 1] = aux;
+          counter += 3;
+          trocou = true;
         }
-        return s + "\n";
+      }
     }
+    return counter;
+  }
 
-    private void redimensionaVetor(int novoTamanho) {
-        int[] temp = new int[novoTamanho];
-        for (int i = 0; i < ocupacao; i++)
-            temp[i] = vetor[i];
-        vetor = temp;
-    }
-
-    public int getTamanho() {
-        return vetor.length;
-    }
-
-    public void esvaziaVetor() {
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = 0;
+  public long selectionsort() {
+    long counter = 0;
+    for (int i = 0; i < vetor.length - 1; ++i) {
+      int min = i;
+      for (int j = i + 1; j < vetor.length; ++j) {
+        counter++;
+        if (vetor[j] < vetor[min]) {
+          min = j;
+          counter++;
         }
-        ocupacao = 0;
+      }
+
+      if (min != i) {
+        int x = vetor[i];
+        vetor[i] = vetor[min];
+        vetor[min] = x;
+        counter += 3;
+      }
     }
+    return counter;
+  }
 
-    public void preencheVetor() {
-        Random random = new Random();
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = random.nextInt(vetor.length * 10);
-        }
-        ocupacao = vetor.length;
+  // Alterado de long para void
+  public void insertionSort() {
+    // long counter = 0;
+    for (int j = 1; j < vetor.length; ++j) {
+      int x = vetor[j];
+      int i;
+      for (i = j - 1; i >= 0 && vetor[i] > x; --i) {
+        // counter++;
+        vetor[i + 1] = vetor[i];
+      }
+      vetor[i + 1] = x;
+      // counter++;
     }
+    // return counter;
+  }
 
-    public long bubbleSort() {
-        long counter = 0;
-        boolean trocou = true;
-        for (int i = 0; i < ocupacao - 1 && trocou; i++) { 
-            trocou = false; 
-            for (int j = 0; j < ocupacao - i - 1; j++) {
-                counter++;
-                if (vetor[j] > vetor[j + 1]) {
-                    int aux = vetor[j];
-                    vetor[j] = vetor[j + 1];
-                    vetor[j + 1] = aux;
-                    counter+=3; 
-                    trocou = true;
-                }
-            }
-        }
-        return counter;
-    }
+  public long buscaLinear(int elemento) {
+    long contadorLinear = 0;
 
-    public long selectionsort() {
-        long counter = 0;
-        for (int i = 0; i < vetor.length - 1; ++i) {
-            int min = i;
-            for (int j = i + 1; j < vetor.length; ++j) {
-                counter++;
-                if (vetor[j] < vetor[min]) {
-                    min = j;
-                    counter++;
-                }                
-            }
-
-            if (min != i) {
-                int x = vetor[i];
-                vetor[i] = vetor[min];
-                vetor[min] = x;
-                counter += 3;
-            }
-        }
-        return counter;
-    }
-    
-    public long insertionSort() {
-        long counter = 0;
-        for (int j = 1; j < vetor.length; ++j) {
-            int x = vetor[j];
-            int i;
-            for (i = j - 1; i >= 0 && vetor[i] > x; --i) {
-                counter++;
-                vetor[i + 1] = vetor[i];
-            }
-            vetor[i + 1] = x;
-            counter++;
-        }
-        if (counter == 0) {
-            System.out.println("Vetor já ordenado");
-        }
-        return counter;
-    }
-
-    public long buscaLinear(int elemento) {
-        long contadorLinear = 0;
-
-        for (int i = 0; i < this.vetor.length; i++) {
-            contadorLinear++;
-            if (this.vetor[i] == elemento) {
-                // System.out.prlongln(i);
-                return contadorLinear;
-            }
-        }
+    for (int i = 0; i < this.vetor.length; i++) {
+      contadorLinear++;
+      if (this.vetor[i] == elemento) {
+        // System.out.prlongln(i);
         return contadorLinear;
+      }
     }
+    return contadorLinear;
+  }
 
-    public long buscaBinaria(int elemento) {
-        long contadorBinario = 0;
+  int partition(int p, int r) {
+    int x = vetor[r];
+    int i = p - 1;
+    for (int j = p; j < r; j++) {
+      if (vetor[j] <= x) {
+        int aux = vetor[++i];
+        vetor[i] = vetor[j];
+        vetor[j] = aux;
+      }
+    }
+    int aux = vetor[r];
+    vetor[r] = vetor[++i];
+    vetor[i] = aux;
+    return i;
+  }
 
-        int inicio = 0;
-        int fim = this.vetor.length - 1;
-        while (inicio <= fim) {
-            int meio = ((inicio + fim) / 2);
-            contadorBinario++;
-            if (this.vetor[meio]  == elemento ) {
-                return contadorBinario;
-            }
-            contadorBinario++;
-            if (this.vetor[meio] < elemento ) {
-                inicio = meio + 1;
-            } else {
-                fim = meio - 1;
-            }
-        }
+  public void quickSort(int p, int r) {
+    if (p < r) {
+      int q = partition(p, r);
+      quickSort(p, q - 1);
+      quickSort(q + 1, r);
+    }
+  }
+
+  public long buscaBinaria(int elemento) {
+    long contadorBinario = 0;
+
+    int inicio = 0;
+    int fim = this.vetor.length - 1;
+    while (inicio <= fim) {
+      int meio = ((inicio + fim) / 2);
+      contadorBinario++;
+      if (this.vetor[meio] == elemento) {
         return contadorBinario;
+      }
+      contadorBinario++;
+      if (this.vetor[meio] < elemento) {
+        inicio = meio + 1;
+      } else {
+        fim = meio - 1;
+      }
     }
+    return contadorBinario;
+  }
 }
 
 class VetorVazioException extends RuntimeException {
-    public VetorVazioException(String msg) {
-        super(msg);
-    }
+  public VetorVazioException(String msg) {
+    super(msg);
+  }
 }
--- ATIVIDADES/atividade1/Teste.java
+++ ATIVIDADES/atividade1/Teste.java
@@ -1,158 +1,184 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class Teste {
   public static void main(String[] args) {
     // vamos testar a eficácia dos métodos de ordenação
+    // Scanner scanner = new Scanner(System.in);
+    // int tamanho = 0;
+    // boolean segue = true;
+    // do {
+    // System.out.println("\n\nSelecione um tamanho: \n");
+    // System.out.println("[1] - 100m\n");
+    // System.out.println("[2] - 200m\n");
+    // System.out.println("[3] - 400m\n");
+    // System.out.println("[4] - 800m\n");
+    // System.out.println("[5] - 1,6M\n");
+    // System.out.println("cancelar - 0\n");
+
+    // System.out.println("Opcao: ");
+    // tamanho = scanner.nextInt();
+
+    // switch (tamanho) {
+    // case 1:
+    // tamanho = 100000;
+    // break;
+    // case 2:
+    // tamanho = 200000;
+    // break;
+    // case 3:
+    // tamanho = 400000;
+    // break;
+    // case 4:
+    // tamanho = 800000;
+    // break;
+    // case 5:
+    // tamanho = 1600000;
+    // break;
+    // default:
+    // tamanho = 0;
+    // System.out.println("Fim das operacoes");
+    // segue = false;
+    // break;
+    // }
+    // ;
+
+    // if (!segue)
+    // break;
+
+    // System.out.println("\nTAMANHO DO VETOR: " + tamanho);
+    // NossoVetor vetor = new NossoVetor(tamanho);
+
+    // System.out.println("Selecione um sort:\n");
+    // System.out.println("[1] Selection Sort\n");
+    // System.out.println("[2] Insertion Sort\n");
+    // System.out.println("[3] Bubble Sort\n");
+    // System.out.println("[4] Cancelar\n");
+
+    // System.out.println("Opção: ");
+    // int sort = scanner.nextInt();
+
+    // long somaTrocas = 0;
+    // long somaTempo = 0;
+    // long somaLinear = 0;
+    // long somaBinaria = 0;
+    // long tempoInicial = System.currentTimeMillis();
+    // double numIteracoes = 30.0;
+    // if (tamanho == 1600000) {
+    // System.out.println("vetor de 1.6M !!");
+    // numIteracoes = 10.0;
+    // }
+
+    // switch (sort) {
+    // case 1:
+    // System.out.println("SELECTION SORT");
+    // for (int i = 0; i < numIteracoes; i++) {
+    // System.out.println("\n" + (i + 1) + ")");
+    // long buscaLi = vetor.buscaLinear(500000);
+    // somaLinear += buscaLi;
+    // System.out.println("Busca Linear: " + buscaLi);
+    // vetor.esvaziaVetor();
+    // vetor.preencheVetor();
+    // long tempoInicialOrdenacao = System.currentTimeMillis();
+    // long iteracoes = vetor.selectionsort();
+    // long buscaBi = vetor.buscaBinaria(500000);
+    // System.out.println("Busca Binaria: " + buscaBi);
+    // somaBinaria += buscaBi;
+    // System.out.println("\nQuantidade de trocas: " + iteracoes);
+    // long tempoFinalOrdenacao = System.currentTimeMillis();
+    // System.out.println("Tempo de ordenacao: " + (tempoFinalOrdenacao -
+    // tempoInicialOrdenacao) + "ms");
+    // somaTrocas += iteracoes;
+    // somaTempo += tempoFinalOrdenacao - tempoInicialOrdenacao;
+    // }
+    // break;
+    // case 2:
+    // System.out.println("INSERTION SORT");
+    // for (int i = 0; i < numIteracoes; i++) {
+    // System.out.println("\n" + (i + 1) + ")");
+    // long buscaLi = vetor.buscaLinear(500000);
+    // somaLinear += buscaLi;
+    // System.out.println("Busca Linear: " + buscaLi);
+    // vetor.esvaziaVetor();
+    // vetor.preencheVetor();
+    // long tempoInicialOrdenacao = System.currentTimeMillis();
+    // long iteracoes = vetor.insertionSort();
+    // long buscaBi = vetor.buscaBinaria(500000);
+    // System.out.println("Busca Binaria: " + buscaBi);
+    // somaBinaria += buscaBi;
+    // System.out.println("\nQuantidade de trocas: " + iteracoes);
+    // long tempoFinalOrdenacao = System.currentTimeMillis();
+    // System.out.println("Tempo de ordenacao: " + (tempoFinalOrdenacao -
+    // tempoInicialOrdenacao) + "ms");
+    // somaTrocas += iteracoes;
+    // somaTempo += tempoFinalOrdenacao - tempoInicialOrdenacao;
+    // }
+    // break;
+    // case 3:
+    // System.out.println("BUBBLE SORT");
+    // for (int i = 0; i < numIteracoes; i++) {
+    // System.out.println("\n" + (i + 1) + ")");
+    // long buscaLi = vetor.buscaLinear(500000);
+    // somaLinear += buscaLi;
+    // System.out.println("Busca Linear: " + buscaLi);
+    // vetor.esvaziaVetor();
+    // vetor.preencheVetor();
+    // long tempoInicialOrdenacao = System.currentTimeMillis();
+    // long iteracoes = vetor.bubbleSort();
+    // long buscaBi = vetor.buscaBinaria(500000);
+    // System.out.println("Busca Binaria: " + buscaBi);
+    // somaBinaria += buscaBi;
+    // System.out.println("\nQuantidade de trocas: " + iteracoes);
+    // long tempoFinalOrdenacao = System.currentTimeMillis();
+    // System.out.println("Tempo de ordenacao: " + (tempoFinalOrdenacao -
+    // tempoInicialOrdenacao) + "ms");
+    // somaTrocas += iteracoes;
+    // somaTempo += tempoFinalOrdenacao - tempoInicialOrdenacao;
+    // }
+    // break;
+    // default:
+    // System.out.println("Fim das operacoes");
+    // segue = false;
+    // break;
+    // }
+
+    // if (!segue) break;
+
+    // long tempoFinal = System.currentTimeMillis();
+    // double tempoTotalSegundos = (tempoFinal - tempoInicial)/1000.0;
+    // System.out.printf("\nMedia de trocas: " + (somaTrocas / (long) numIteracoes )
+    // );
+    // double mediaSegundos = (somaTempo/numIteracoes)/1000.0;
+    // System.out.printf("\nTempo total: %.1f segundos\n", tempoTotalSegundos);
+    // System.out.printf("Media para completar: %.1f segundos\n", mediaSegundos);
+    // System.out.printf("Media Busca Linear: %.1f\n", (somaLinear / numIteracoes));
+    // System.out.printf("Media Busca Binaria: %.1f\n", (somaBinaria /
+    // numIteracoes));
+
+    // } while (tamanho != 0);
+    // scanner.close();
+    System.out.println("Digite o tamanho do vetor: ");
     Scanner scanner = new Scanner(System.in);
-    int tamanho = 0;
-    boolean segue = true;
-    do {
-      System.out.println("\n\nSelecione um tamanho: \n");
-      System.out.println("[1] - 100m\n");
-      System.out.println("[2] - 200m\n");
-      System.out.println("[3] - 400m\n");  
-      System.out.println("[4] - 800m\n");
-      System.out.println("[5] - 1,6M\n");
-      System.out.println("cancelar - 0\n");
-      
-      System.out.println("Opcao: ");
-      tamanho = scanner.nextInt();
+    int t = scanner.nextInt();
+    while (t > 0) {
+      NossoVetor v = new NossoVetor(t);
+      v.preencheVetor();
+      long ini = new Date().getTime();
+      v.insertionSort();
+      long fim = new Date().getTime();
+      System.out.println("Vetor ordenado: " + v);
+      System.out.println("Insertion, para o tamanho " + t + " levou " + (fim - ini) + "ms");
 
-      switch (tamanho) {
-        case 1:
-          tamanho = 100000;
-          break;
-        case 2:
-          tamanho = 200000;
-          break;
-        case 3:
-          tamanho = 400000;
-          break;
-        case 4:
-          tamanho = 800000;
-          break;
-        case 5:
-          tamanho = 1600000;
-          break;
-        default:
-          tamanho = 0;
-          System.out.println("Fim das operacoes");
-          segue = false;
-          break;
-      }
-      ;
-      
-      if (!segue)
-        break;
-      
-      System.out.println("\nTAMANHO DO VETOR: " + tamanho);
-      NossoVetor vetor = new NossoVetor(tamanho);
+      v.preencheVetor();
+      ini = new Date().getTime();
+      v.quickSort(0, t - 1);
+      fim = new Date().getTime();
+      System.out.println("Vetor ordenado: " + v);
+      System.out.println("Quick, para o tamanho " + t + " levou " + (fim - ini) + "ms");
 
-      System.out.println("Selecione um sort:\n");
-      System.out.println("[1] Selection Sort\n");
-      System.out.println("[2] Insertion Sort\n");
-      System.out.println("[3] Bubble Sort\n");
-      System.out.println("[4] Cancelar\n");
+      System.out.println("Digite o tamanho do vetor: ");
+      t = scanner.nextInt();
+    }
 
-      System.out.println("Opção: ");
-      int sort = scanner.nextInt();
-      
-
-      long somaTrocas = 0;
-      long somaTempo = 0;
-      long somaLinear = 0;
-      long somaBinaria = 0;
-      long tempoInicial = System.currentTimeMillis();
-      double numIteracoes = 30.0;
-      if (tamanho == 1600000) {
-        System.out.println("vetor de 1.6M !!");
-        numIteracoes = 10.0;
-      }
-
-      switch (sort) {
-        case 1:
-          System.out.println("SELECTION SORT");
-          for (int i = 0; i < numIteracoes; i++) {
-            System.out.println("\n" + (i + 1) + ")");
-            long buscaLi = vetor.buscaLinear(500000);
-            somaLinear += buscaLi;
-            System.out.println("Busca Linear: " + buscaLi);
-            vetor.esvaziaVetor();
-            vetor.preencheVetor();
-            long tempoInicialOrdenacao = System.currentTimeMillis();
-            long iteracoes = vetor.selectionsort();
-            long buscaBi = vetor.buscaBinaria(500000);
-            System.out.println("Busca Binaria: " + buscaBi);
-            somaBinaria += buscaBi;
-            System.out.println("\nQuantidade de trocas: " + iteracoes);
-            long tempoFinalOrdenacao = System.currentTimeMillis();
-            System.out.println("Tempo de ordenacao: " + (tempoFinalOrdenacao - tempoInicialOrdenacao) + "ms");
-            somaTrocas += iteracoes;
-            somaTempo += tempoFinalOrdenacao - tempoInicialOrdenacao;
-          }
-          break;
-        case 2:
-          System.out.println("INSERTION SORT");
-          for (int i = 0; i < numIteracoes; i++) {
-            System.out.println("\n" + (i + 1) + ")");
-            long buscaLi = vetor.buscaLinear(500000);
-            somaLinear += buscaLi;
-            System.out.println("Busca Linear: " + buscaLi);
-            vetor.esvaziaVetor();
-            vetor.preencheVetor();
-            long tempoInicialOrdenacao = System.currentTimeMillis();
-            long iteracoes = vetor.insertionSort();
-            long buscaBi = vetor.buscaBinaria(500000);
-            System.out.println("Busca Binaria: " + buscaBi);
-            somaBinaria += buscaBi;
-            System.out.println("\nQuantidade de trocas: " + iteracoes);
-            long tempoFinalOrdenacao = System.currentTimeMillis();
-            System.out.println("Tempo de ordenacao: " + (tempoFinalOrdenacao - tempoInicialOrdenacao) + "ms");
-            somaTrocas += iteracoes;
-            somaTempo += tempoFinalOrdenacao - tempoInicialOrdenacao;
-          }
-          break;
-        case 3:
-          System.out.println("BUBBLE SORT");
-          for (int i = 0; i < numIteracoes; i++) {
-            System.out.println("\n" + (i + 1) + ")");
-            long buscaLi = vetor.buscaLinear(500000);
-            somaLinear += buscaLi;
-            System.out.println("Busca Linear: " + buscaLi);
-            vetor.esvaziaVetor();
-            vetor.preencheVetor();
-            long tempoInicialOrdenacao = System.currentTimeMillis();
-            long iteracoes = vetor.bubbleSort();
-            long buscaBi = vetor.buscaBinaria(500000);
-            System.out.println("Busca Binaria: " + buscaBi);
-            somaBinaria += buscaBi;
-            System.out.println("\nQuantidade de trocas: " + iteracoes);
-            long tempoFinalOrdenacao = System.currentTimeMillis();
-            System.out.println("Tempo de ordenacao: " + (tempoFinalOrdenacao - tempoInicialOrdenacao) + "ms");
-            somaTrocas += iteracoes;
-            somaTempo += tempoFinalOrdenacao - tempoInicialOrdenacao;
-          }
-          break;
-        default:
-          System.out.println("Fim das operacoes");
-          segue = false;
-          break;
-      }
-
-      if (!segue) break;
-
-      long tempoFinal = System.currentTimeMillis();
-      double tempoTotalSegundos = (tempoFinal - tempoInicial)/1000.0;
-      System.out.printf("\nMedia de trocas: " + (somaTrocas / (long) numIteracoes ) );
-      double mediaSegundos = (somaTempo/numIteracoes)/1000.0;
-      System.out.printf("\nTempo total: %.1f segundos\n", tempoTotalSegundos);
-      System.out.printf("Media para completar: %.1f segundos\n", mediaSegundos);
-      System.out.printf("Media Busca Linear: %.1f\n", (somaLinear / numIteracoes));
-      System.out.printf("Media Busca Binaria: %.1f\n", (somaBinaria / numIteracoes));
-
-
-      
-    } while (tamanho != 0);
-    scanner.close();
   }
 }
