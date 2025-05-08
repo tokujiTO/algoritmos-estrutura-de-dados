90654a0eb94963bc2e7001f604f5cde89edb77dc
tokujiTO <tiagomassuda123@gmail.com>
2025-05-08 18:05:26 -0300

    add entryTime to document

--- ATIVIDADES/atividade2/Documento.java
+++ ATIVIDADES/atividade2/Documento.java
@@ -1,18 +1,36 @@
 public class Documento implements Comparable<Documento> {
   private String nome;
   private String conteudo;
+  private int horaEntrada;
 
-  public Documento(String nome, String conteudo) {
-    this.nome = nome;
+  public Documento(String nome, String conteudo, int horaEntrada) {
+    this.nome = nome;s
     this.conteudo = conteudo;
+    this.horaEntrada = horaEntrada;
   }
 
   public String getNome() {
     return nome;
   }
 
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
   public String getConteudo() {
     return conteudo;
+  }
+
+  public void setConteudo(String conteudo) {
+    this.conteudo = conteudo;
+  }
+
+  public int getHoraEntrada() {
+    return horaEntrada;
+  }
+
+  public void setHoraEntrada(int horaEntrada) {
+    this.horaEntrada = horaEntrada;
   }
 
   @Override
